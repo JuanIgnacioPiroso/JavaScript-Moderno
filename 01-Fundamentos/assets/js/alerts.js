8591ec4f911928ac5f1aa283fbc0e57212061819
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2023-02-07 15:36:04 -0300

    Introduccion a JavaScript y la consola

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,9 @@
+// alert("Hola mundo");
+
+// let nombre = prompt("Cual es tu nombre?");
+
+// console.log("****" + nombre + "****");
+
+const seleccion = confirm("¿Quiere borrar esto? ");
+
+console.log(seleccion);
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,24 @@
+// console.log(console.log('Hola Mundo'));
+
+// alert("Hola desde app.js");
+
+console.log(miNuevoNombre);
+
+let a = 10,
+  b = 20,
+  c = "Hola",
+  d = "Spiderman",
+  x = a + b;
+
+const saludo = c + d;
+
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = "Hola de nuevo";
+
+// console.table({ a, b, c, d, x });
+
+
+let miNuevoNombre = "Juancho";
