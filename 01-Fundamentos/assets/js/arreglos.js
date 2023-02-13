dd1fcc3ae783b5a73b83667284c264e112a48a0c
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2023-02-13 11:18:02 -0300

    arreglos-terminado

---
+++ 01-Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,11 @@
+// const arr = new Array(10); - Forma de hacer un array poco comun
+
+// const arr = [];
+
+let videoJuegos = ["Mario 3", "Megaman", "Chrono Trigger"];
+console.log({ videoJuegos });
+
+let arregloCosas = [true, 123, "Juan", 1 + 2,function(){},()=>{},{a: 1},['X','Megaman','Zero','Dr.Light']];
+
+console.log(arregloCosas[2]);
+console.log(arregloCosas[7][3]);
