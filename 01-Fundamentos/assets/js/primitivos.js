3c2af49e6d73c3068f8e396ee42b4d19581fb505
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2023-02-13 10:27:47 -0300

    Tipos de primitivos

---
+++ 01-Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,18 @@
+/* Strings */
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+nombre = 'Ben Parker';
+console.log(nombre);
+/* Strings */
+
+/* Booleanos */
+let esMarvel = true;
+console.log(typeof esMarvel);
+/* Booleanos */
+
+/* Numeros */
+let edad = 33;
+console.log(typeof edad);
+/* Numeros */
+
