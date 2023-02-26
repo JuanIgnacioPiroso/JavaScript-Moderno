9ece3c312b39181db921a5f231e22c2ee3681e2a
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2023-02-25 23:10:08 -0300

    code cleaning

--- 02-Proyecto Blackjack/assets/js/juego.js
+++ 02-Proyecto Blackjack/assets/js/juego.js
@@ -9,7 +9,6 @@ let deck = [];
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "J", "Q", "K"];
 
-
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tipos) {
