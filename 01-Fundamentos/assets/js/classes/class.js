f9b59edfab44af857d55807e66f2c92f1b2d1a03
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2023-03-01 18:48:41 -0300

    gets and sets

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -2,11 +2,20 @@ class Persona {
   nombre = "";
   codigo = "";
   frase = "";
+  comida = "";
 
   constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase') {
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+  }
+
+  set setComidaFavorita(comida){
+    this.comida = comida.toUpperCase();
+  }
+
+  get getComidaFavorita(){
+    return `La comida favorita de ${this.nombre } es ${this.comida}`;
   }
 
   quienSoy(){
@@ -21,7 +30,12 @@ class Persona {
 const spiderman = new Persona('Peter Parker','Spiderman','La de peter parker no falla');
 const ironman = new Persona('Tony Stark','IronMan','Soy ironman');
 
-console.log(spiderman);
-console.log(ironman);
-
 spiderman.miFrase();
+
+spiderman.setComidaFavorita = 'Pie de cereza';
+
+console.log(spiderman.getComidaFavorita);
+console.log(spiderman);
+
+
+
