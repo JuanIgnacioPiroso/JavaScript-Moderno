3062d4079509a35b44dde2961ac269c232c44918
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-14 02:22:13 -0300

    promises

--- 05-Advanced code/src/concepts/03-Promises.js
+++ 05-Advanced code/src/concepts/03-Promises.js
@@ -5,7 +5,19 @@ import { heroes } from "../data/heroes";
  * @param {HTMLDivElement} element
  */
 export const promiseComponent = (element) => {
-  console.log("promiseComponent");
+  const renderHero = (hero) => {
+    element.innerHTML = hero.name;
+  };
+
+  const renderError = (error) => {
+    element.innerHTML = `
+    <h1>Error:</h1>
+    <h3>${error}</h3>`;
+  };
+
+  const id1 = "5d86371fd55e2e2a30fe1ccb2";
+
+  findHero(id1).then(renderHero).catch(renderError);
 };
 
 /**
