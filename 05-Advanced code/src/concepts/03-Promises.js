516d857c25bc4fb8e5f4726bb0a66e36adcc6819
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-15 15:28:31 -0300

    Promise hell

--- 05-Advanced code/src/concepts/03-Promises.js
+++ 05-Advanced code/src/concepts/03-Promises.js
@@ -9,15 +9,30 @@ export const promiseComponent = (element) => {
     element.innerHTML = hero.name;
   };
 
+  const renderTwoHeroes = (hero1, hero2) => {
+    element.innerHTML = `
+    <h3>${hero1.name}</h3>
+    <h3>${hero2.name}</h3>`;
+  };
+
   const renderError = (error) => {
     element.innerHTML = `
     <h1>Error:</h1>
     <h3>${error}</h3>`;
   };
 
-  const id1 = "5d86371fd55e2e2a30fe1ccb2";
+  const id1 = "5d86371fd55e2e2a30fe1ccb";
+  const id2 = "5d86371fd55e2e2a30fe1cc4";
 
-  findHero(id1).then(renderHero).catch(renderError);
+  findHero(id1)
+    .then((hero) => {
+      findHero(id2)
+        .then((hero2) => {
+          renderTwoHeroes(hero, hero2);
+        })
+        .catch(renderError);
+    })
+    .catch(renderError);
 };
 
 /**
