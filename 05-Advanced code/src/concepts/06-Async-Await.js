389e5953acc84ea0b575729ae37c9a8c427bb73d
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-15 16:49:45 -0300

    exceptions

--- 05-Advanced code/src/concepts/06-Async-Await.js
+++ 05-Advanced code/src/concepts/06-Async-Await.js
@@ -8,10 +8,14 @@ export const asyncAwaitComponent = async (element) => {
   const id1 = "5d86371fd55e2e2a30fe1ccb2";
   const id2 = "5d86371f233c9f2425f16916";
 
-  const hero1 = await findHero(id1);
-  const hero2 = await findHero(id2);
+  try {
+    const hero1 = await findHero(id1);
+    const hero2 = await findHero(id2);
 
-  element.innerHTML = `${hero1.name} / ${hero2.name} `;
+    element.innerHTML = `${hero1.name} / ${hero2.name} `;
+  } catch (error) {
+    element.innerHTML = error;
+  }
 };
 
 const findHero = async (id) => {
