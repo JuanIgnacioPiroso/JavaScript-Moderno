0eb6cfba4ce2c1e76ab0710173387394c631a211
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-15 16:20:48 -0300

    async functions

--- 05-Advanced code/src/concepts/05-Async.js
+++ 05-Advanced code/src/concepts/05-Async.js
@@ -1,9 +1,23 @@
+import { heroes } from "../data/heroes";
+
 /**
  *
  * @param {HTMLDivElement} element
  */
 export const asyncComponent = (element) => {
-    console.log("asyncComponent");
-  };
-  
-  
+  const id1 = "5d86371fd55e2e2a30fe1ccb2";
+
+  findHero(id1)
+    .then((name) => (element.innerHTML = name))
+    .catch((error) => (element.innerHTML = error));
+};
+
+/**
+ *
+ * @param {String} id
+ */
+const findHero = async (id) => {
+  const hero = heroes.find((hero) => hero.id === id);
+
+  return hero.name;
+};
