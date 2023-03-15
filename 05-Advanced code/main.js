b7a4f1ec1a142355ec9fa96872b9afc486ebe7d0
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-15 19:05:44 -0300

    for-await added

--- 05-Advanced code/main.js
+++ 05-Advanced code/main.js
@@ -7,6 +7,7 @@ import { promiseRaceComponent } from "./src/concepts/04-Promise-race";
 import { asyncComponent } from "./src/concepts/05-Async";
 import { asyncAwaitComponent } from "./src/concepts/06-Async-Await";
 import { asyncAwait2Component } from "./src/concepts/07-Async-Await";
+import { forAwaitComponent } from "./src/concepts/08-For-Await";
 
 document.querySelector("#app").innerHTML = `
   <div>
@@ -31,4 +32,5 @@ const element = document.querySelector(".card");
 //promiseRaceComponent(element);
 //asyncComponent(element);
 //asyncAwaitComponent(element);
-asyncAwait2Component(element);
+//asyncAwait2Component(element);
+//forAwaitComponent(element);
