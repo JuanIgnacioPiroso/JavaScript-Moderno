bb54445ced569fb9a005c1c843dc07feec194fec
Juan Ignacio <jpiroso@frba.utn.edu.ar>
2023-03-16 00:15:37 -0300

    http get api

---
+++ 06-Http-app/src/breakingbad/breakingbad-app.js
@@ -0,0 +1,34 @@
+/**
+ * @return {Object} Quote information
+ */
+const fetchQuote = async () => {
+  const response = await fetch("https://api.breakingbadquotes.xyz/v1/quotes");
+
+  const data = await response.json();
+
+  console.log(data[0]);
+  return data[0];
+};
+
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const BreakingBadApp = async (element) => {
+  document.querySelector("#app-title").innerHTML = "Breakingbad App";
+  element.innerHTML = "Loading...";
+  //const quote = await fetchQuote();
+
+  const quoteLabel = document.createElement("blockquote");
+  const authoLabel = document.createElement("h3");
+  const nextQuoteButton = document.createElement("button");
+  nextQuoteButton.innerText = "Next Quote";
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.quote;
+    authoLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authoLabel, nextQuoteButton);
+  };
+
+  fetchQuote().then(renderQuote);
+};
